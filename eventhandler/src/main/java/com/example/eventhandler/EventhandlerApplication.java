67821306e7b91bafa138e3d0d4688b59cb04d68b
THARUNBASKAR26 <133472296+THARUNBASKAR26@users.noreply.github.com>
2026-03-27 11:03:56 +0530

    Initial main commit

---
+++ eventhandler/src/main/java/com/example/eventhandler/EventhandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhandlerApplication.class, args);
+	}
+
+}
